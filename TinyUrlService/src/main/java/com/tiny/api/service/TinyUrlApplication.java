099f31262a3f1846998953839685f72a784a10f5
optimasqaud <33160067+optimasquad@users.noreply.github.com>
2020-09-26 21:48:45 +0530

    Adding the initial changes
    Adding the initial changes

---
+++ TinyUrlService/src/main/java/com/tiny/api/service/TinyUrlApplication.java
@@ -0,0 +1,17 @@
+package com.tiny.api.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JATIN MAHAJAN
+ *
+ */
+@SpringBootApplication
+public class TinyUrlApplication {
+
+	public static void main(String args[]) {
+
+		SpringApplication.run(TinyUrlApplication.class, args);
+	}
+}
---
+++ TinyUrlService/src/main/java/com/tiny/api/service/config/AppConfig.java
@@ -0,0 +1,15 @@
+
+package com.tiny.api.service.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author JATIN MAHAJAN
+ *
+ */
+@Configuration
+@ComponentScan("com.tiny")
+public class AppConfig {
+
+}
